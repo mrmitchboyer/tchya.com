7d8fcabd270ca656bf0ccd5ede5d7c4072bc730b
mrmitchboyer <mitch@mitchboyer.com>
2015-01-14 16:13:17 -0500

    sessions fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
-  set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
+  set :session_secret, "my_application_secret"
+  enable :sessions
 
   get '/' do
     erb :index
   end
+  
 end
